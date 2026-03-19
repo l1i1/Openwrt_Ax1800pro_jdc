cd4a6daf9db0c49f4101f23fcf72ed76b6fa9645
l.l <vhb.main@qq.com>
2026-03-19 17:58:40 +0800

    fix: remove trojan-plus (Boost library issue)

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,19 +10,15 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-
-
-
-#修改golang版本
+#淇敼golang鐗堟湰
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 #./scripts/feeds install -a -f
 
-# 修复 mbedtls 与 GCC 14 + musl fortify 的兼容性问题
-# 参考: https://github.com/openwrt/openwrt/issues/15351
-# 在 cmake.mk 之后插入 TARGET_CFLAGS filter，移除 FORTIFY_SOURCE
-sed -i '/include.*cmake\.mk/a TARGET_CFLAGS:=$(filter-out -D_FORTIFY_SOURCE=%,$(TARGET_CFLAGS))' package/libs/mbedtls/Makefile
+# 淇 mbedtls 涓?GCC 14 + musl fortify 鐨勫吋瀹规€ч棶棰?sed -i "/include.*cmake\.mk/a TARGET_CFLAGS:=`$(filter-out -D_FORTIFY_SOURCE=%,`$(TARGET_CFLAGS))" package/libs/mbedtls/Makefile
 
-# 移除 ddns-go (需要 Go 1.25+，当前环境为 Go 1.24)
+# 绉婚櫎 ddns-go (闇€瑕?Go 1.25+锛屽綋鍓嶇幆澧冧负 Go 1.24)
 rm -rf feeds/kenzo/ddns-go
 
+# 绉婚櫎 trojan-plus (Boost system 搴撶紪璇戦棶棰?
+rm -rf feeds/small/trojan-plus
